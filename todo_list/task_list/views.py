5305845a07e46d0a3d01434f2c2a6439aab1b416
KennyDevz <kenneth.kenn2004@gmail.com>
2025-09-21 10:56:08 +0800

    feature(task_list): complete app setup

--- todo_list/task_list/views.py
+++ todo_list/task_list/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created')
+    return render(request, 'task_list/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'task_list/add_task.html')
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
